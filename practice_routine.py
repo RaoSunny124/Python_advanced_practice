6b072949a8dac9c60fb76ea1438e114ae899821e
Hassan <hr8581492gmail.com>
2024-12-04 10:48:49 -0700

    first commit

---
+++ practice_routine.py
@@ -0,0 +1,23 @@
+#  Variable 
+#  Data type (primitive or non-primitive)
+# string(/n /t, upper lower , single double triple quotes , etc)
+#  Type casting
+#  f-string
+#  List (slicing etc.)
+#  Tuple
+#  Dictionary 
+#  Arithmetic symbols
+#  input function
+#  If ,elif or else statement
+#  Nested statement
+#  Loops(for , while)
+#  Function (user defined , computer defined)
+#  OOPS 
+#  .md file
+#  Lamda function 
+#  file handling 
+#  Libraries (mypy,pandas) 
+
+
+
+ 
